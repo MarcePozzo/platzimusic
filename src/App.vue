038ebdfab2ed9faf73c6718153cf9c606f1b7dad
Marcelo <mpozzo@trimix.com.ar>
2018-07-03 13:06:14 -0300

    otro

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='https://marcepozzo.github.io/platzimusic/dist/logo.png')
     h1 PlatziMusic
     select(v-model="paisSeleccionado")
       option(v-for="pais in paises" v-bind:value="pais.value") {{pais.name}}
